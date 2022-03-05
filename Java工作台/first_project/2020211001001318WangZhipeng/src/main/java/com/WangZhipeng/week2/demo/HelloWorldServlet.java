f0c0e7d93002a4970f8ead26cb16d82e7fa83c8c
surdream <2548002489@qq.com>
2022-03-05 21:11:38 +0800

    First Commit

---
+++ Java工作台/first_project/2020211001001318WangZhipeng/src/main/java/com/WangZhipeng/week2/demo/HelloWorldServlet.java
@@ -0,0 +1,30 @@
+package com.WangZhipeng.week2.demo;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class HelloWorldServlet extends HttpServlet {
+    String name="Wangzhipeng";
+    String id="2020211001001318";
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        PrintWriter writer=response.getWriter();
+        response.setContentType("text/html;charset=utf-8");
+        Date date=new Date();
+        writer.println("name: "+name+"<br/>");
+        writer.println("id: "+id+"<br/>");
+        writer.println("Date and Time: "+date.toString()+"<br/>");
+    }
+
+
+
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+}
